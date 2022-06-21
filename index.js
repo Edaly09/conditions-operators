d11ff6900e54a0f6e25d73b28ef22409b40a8111
Edaly09 <edalyminthe@gmail.com>
2022-06-21 13:40:46 +0200

    exos

---
+++ index.js
@@ -0,0 +1,114 @@
+// 01 - Comparaison
+// const test = 143
+// const bis = 219
+
+// if(test >= bis){
+//     console.log("test, est superieur ou égal à bis")
+   
+// }
+// else {
+// console.log("bis est superieur ou égal à test")
+// }
+
+//  02 - Condition
+// const limit = 50 
+// let score = 64
+
+// if(score >= limit){
+//     console.log("ok goog !")
+// }
+// else{
+//     console.log("Oh nooo...")
+// }
+
+// score = 40
+
+// if(score <= limit){
+//     console.log("Attaquez les gars !")
+// }
+// else{
+//     console.log("this good...")
+// }
+
+// ## 03 - Condition II
+// const password = "azerty"
+// const number = "5"
+
+ 
+// if((password.length) > (number.length)){
+//     console.log("The password is secure")
+// }
+
+
+// ## 04 - Condition III
+// const limit = 50
+// let score = 64
+
+// if(score >= limit ){
+//     console.log("Everything is good !")
+// }
+// else{
+//     console.log("Oh nooo...")
+// }
+//  score = 40
+// if(score < limit  || score > limit ){
+//     console.log("Something is goog !")
+// }
+// else{
+//     console.log("Oh nooo...")
+// }
+
+// if(score === limit || score > limit){
+//     console.log("Something is goog  !")
+// }
+// else{
+//     console.log("Nothing is good ...")
+// }
+
+// 05 - Random
+// const max = 6
+//  const min = 1
+
+// const random = Math.floor(Math.random() * max) + min
+
+// if(random >= min){
+//     console.log("Yes I win")
+// }
+// else{
+//     console.log("So close...")
+// }
+
+
+// ## 06 - Month
+// let month = "April"
+
+// switch(month) {
+//     case "january":
+//     console.log("winter")
+//     break
+    
+//     case "feruary":
+//     console.log("spring")
+//     break
+    
+//     case "march":
+//     console.log("edaly")
+//     break
+
+//     case "April":
+//     console.log("summer")
+//     break
+    
+//     case "feruary":
+//     console.log("full")
+//     break
+// }
+
+// ## -7 Rounded but better
+
+let roundedNumber = 3.6
+
+
+// - Créez une variable `roundedNumber` de valeur `3.6`
+// - En utilisant uniquement `Math.floor()`, `Math.ceil()`, des opérations mathématiques et un `if () {} else {}` reproduisez le comportement de `Math.round()`
+// - Changez la valeur de `roundedNumber` pour tester l'algorithme que vous avez créé avec les valuers `3.3`, `3.8` et `12.4` et vérifier que ça marche bien dans tous les cas.
